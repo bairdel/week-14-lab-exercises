c2b0a9e305fdb8c6bfa93271a7f77da39fbc053f
bairdel <beck.sewart@warwick.ac.uk>
2023-02-20 10:01:49 +0000

    removed empty if statement

--- mashup/index.js
+++ mashup/index.js
@@ -1,9 +1,8 @@
 console.log("Hello this is mashup js");
-const myStatement = new Boolean(false);
+var myStatement = new Boolean(false);
 
 if (myStatement == false)
 
 {
   console.log("This is mashup" + "strings");
-  if (true == false) { pass };
 }
